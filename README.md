b7615fc80411df0e7c6a256459e4e13689cf2fb3
Atul <aatul151.prajapati@gmail.com>
2022-10-28 20:41:05 +0530

    git add

---
+++ README.md
@@ -0,0 +1,4 @@
+# Learn_Nodejs
+#author : Nikhil Prajapati
+#download MongoDB & install : https://www.mongodb.com/try/download/community
+
---
+++ app.js
@@ -0,0 +1,49 @@
+require('dotenv').config()
+
+const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const app = express();
+
+//My routes
+
+const contactRoute = require("./routes/contact");
+
+
+//DB connection
+mongoose
+.connect(process.env.DATABASE,{ 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex:true
+}).then(() =>{
+    console.log("DB CONNECTED...");
+}).catch(()=>{
+    console.log("DB GOT OOOPS  {error}_something went wrong");
+})
+    
+
+//Middelewere 
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+
+//My routes 
+app.get('/' , (req,res) => {
+    return res.send("hello world ! Welcome to NodeJs Backend Developer");
+});
+
+app.use("/api", contactRoute);
+
+
+
+//PORT
+const port =  process.env.port || 8000;
+
+//Staring Server
+app.listen(port,()=>{
+    console.log(`App is running on port ${port}`);
+});
